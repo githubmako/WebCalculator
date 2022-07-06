47de44fb92936d635c6fc165c4fefaa096f44111
mako <91590966+githubmako@users.noreply.github.com>
2022-07-06 14:46:18 +0200

    final project

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+
 const liczby = document.querySelectorAll('.liczba')
 const operatory = document.querySelectorAll('.operator')
 const wyczysc = document.querySelector('.wyczysc')
@@ -8,118 +9,136 @@ const wynikPoprzednie = document.querySelector('.poprzednie-dzialanie')
 const wynikAktualne = document.querySelector('.aktualne-dzialanie')
 
 
-var aktualneDzialanie = ''
-var operacja = undefined
-var poprzednieDzialanie = ''
-
-
+let aktualneDzialanie = ''
+let operacja = undefined
+let poprzednieDzialanie = ''
 
 const oblicz = () => {
+  let dzialanie
+  if(!poprzednieDzialanie || !aktualneDzialanie) {
+    return
+  }
 
-    let dzialanie; 
-    if (!poprzednieDzialanie || aktualneDzialanie); 
-    {
-        return; 
-    }
-    
-        const poprzednie = parseFloat(poprzednieDzialanie); 
-        const aktualne = parseFloat(aktualneDzialanie); 
+  const poprzednie = parseFloat(poprzednieDzialanie)
+  const aktualne = parseFloat(aktualneDzialanie)
 
-        if(isNaN(poprzednie) || isNaN(aktualne)){
+  if(isNaN(poprzednie) || isNaN(aktualne)) {
+    return
+  }
 
-            return; 
-        }
+  switch (operacja) {
+    case '+':
+      dzialanie = poprzednie + aktualne
+      break
+      case '-':
+        dzialanie = poprzednie - aktualne
+      break
+      case '×':
+        dzialanie = poprzednie * aktualne
+      break
+      case '÷':
+      if(aktualne === 0)
+      {
+        wyczyscWynik()
+        return
+      }
+        dzialanie = poprzednie / aktualne
+      break
+      case '^':
+        dzialanie = Math.pow(poprzednie, aktualne)
+      break
+      case '%':
+        dzialanie = poprzednie / 100 * aktualne
+      break
+      case '√':
+        dzialanie = Math.pow(poprzednie, 1 / aktualne)
+      break
+      case 'log':
+        dzialanie = Math.log(poprzednie) / Math.log(aktualne)
+      break
+    default:
+      return
+  }
+  aktualneDzialanie = dzialanie
+  operacja = undefined
+  poprzednieDzialanie = ''
 
-        switch(operacja){
-            case '+':
-            dzialanie = poprzednie + aktualne; 
-            break; 
-            case '-':
-            dzialanie = poprzednie - aktualne; 
-            case '&times':
-            dzialanie = poprzednie * aktualne; 
-            break; 
-            case '÷': 
-            dzialanie = poprzednie / aktualne;
-            case '√': 
-            dzialanie = Math.pow(poprzednie, 1/aktualne); 
-            break;
-            default:
-            return
-        }
-        
-}
-
-const zaktualizujWynik = () => {
-
-    wynikAktualne.innerText = aktualneDzialanie; 
-    wynikPoprzednie.innerText = poprzednieDzialanie;
-    if(operacja != null){
-        wynikPoprzednie.innerText = poprzednieDzialanie + operacja;
-
-    }
-    else {
-        wynikPoprzednie.innerText = ""; 
-    }
-
-}
-
-const dodajLiczbe = (liczba) => { 
-    
-    if(liczba === '•')
-    {
-        if(aktualneDzialanie.includes('.'))
-    {
-        return;
-    }
-        liczba = "."
-    }
-    
-    aktualneDzialanie = aktualneDzialanie.toString() + liczba.toString() }
-
-liczby.forEach((liczba) => {
-   
-
-    liczba.addEventListener("click", () => {
-        dodajLiczbe(liczba.innerText)
-        zaktualizujWynik()
-    })
-})
-
-usun.addEventListener("click" , () => {
-usunLiczbe()
-zaktualizujWynik()
-})
-operatory[7].addEventListener("click", () => {
-
-    dodajLiczbe();
-     zaktualizujWynik();
-
-
-})
-
-const usunLiczbe = () => 
-{
-    aktualneDzialanie = aktualneDzialanie.slice(0, -1)
 }
 
 const wybierzOperacje = (operator) => {
+  if(aktualneDzialanie === '') {
+    return
+  }
+  if(poprzednieDzialanie !== '') {
+    const poprzednie = wynikPoprzednie.innerText
+    if(aktualneDzialanie.toString() === '0' &&  poprzednie[poprzednie.length - 1] === '÷') {
+      wyczyscWynik()
+      return
+    }
+    oblicz()
+  }
 
-    if(aktualneDzialanie === "") { return }; 
-    if (poprzednieDzialanie !== '') {oblicz()}; 
-
-    operacja = operator; 
-    poprzednieDzialanie = aktualneDzialanie; 
-    aktualneDzialanie = ""; 
-
+  operacja = operator
+  poprzednieDzialanie = aktualneDzialanie
+  aktualneDzialanie = ''
 }
 
+const dodajLiczbe = (liczba) => {
+  if(liczba === '•') {
+    if(aktualneDzialanie.includes('.')) {
+      return
+    }
+    liczba = '.'
+  }
 
-operatory.forEach((operator) => { 
-    operator.addEventListener("click", () => {
+  aktualneDzialanie = aktualneDzialanie.toString() + liczba.toString()
+}
 
-        wybierzOperacje(operator.innerText); 
-        zaktualizujWynik(); 
+const usunLiczbe = () => {
+  aktualneDzialanie = aktualneDzialanie.toString().slice(0, -1)
+}
 
-    })
-});
+const zaktualizujWynik = () => {
+  wynikAktualne.innerText = aktualneDzialanie
+
+  if(operacja != null) {
+  wynikPoprzednie.innerText = poprzednieDzialanie + operacja
+  } else {
+    wynikPoprzednie.innerText = ''
+  }
+}
+
+const wyczyscWynik = () => {
+  aktualneDzialanie = ''
+  operacja = undefined
+  poprzednieDzialanie = ''
+}
+
+liczby.forEach((liczba) => {
+  liczba.addEventListener('click', () => {
+    dodajLiczbe(liczba.innerText)
+    zaktualizujWynik()
+  })
+})
+
+operatory.forEach((operator) => {
+  operator.addEventListener('click', () => {
+    wybierzOperacje(operator.innerText)
+    zaktualizujWynik()
+  })
+})
+
+rownosc.addEventListener('click', () => {
+  oblicz()
+  zaktualizujWynik()
+})
+
+usun.addEventListener('click', () => {
+  usunLiczbe()
+  zaktualizujWynik()
+})
+
+wyczysc.addEventListener('click', () => {
+  wyczyscWynik()
+  zaktualizujWynik()
+})
